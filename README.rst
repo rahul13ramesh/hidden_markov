deff66a474dfcc586370a674c67840f1d7c9d2a6
Red-devilz <rahul.ramesh10@yahoo.co.in>
2016-07-17 15:03:08 +0530

    added readme

---
+++ README.rst
@@ -0,0 +1,9 @@
+Hidden Markov Model
+-------------------
+
+An implementation of Viterbi Algorithm, Forward algorithm and the Baum Welch Algorithm. The computations are done via matrices to imporve algorithm runtimes. The hmm class takes emission, start probabilities and transition probabilities as matrix inputs and appropriate outputs matrices are produced.
+
+Usage ::
+
+    >>> import hmm
+    >>> To be completed 
--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='hidden_markov',
       version='0.1',
-      description='Implementation of Hidden markov model in discrete and continuous domains.',
+      description='Implementation of Hidden markov model in discrete domain.',
       url='http://github.com/Red-devilz/hidden_markov',
-      author='Red-devilz',
+      author='Rahul',
       author_email='',
       license='MIT',
       packages=['hidden_markov'],
+      install_requires=[
+          'numpy',
+      ],
+
+      long_description=readme(),
+
+      keywords='hmm markov model',
+
       zip_safe=False)
