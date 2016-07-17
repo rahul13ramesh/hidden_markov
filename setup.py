7930bbfebb4547a92009b88859bd39c0fa89b82b
Red-devilz <rahul.ramesh10@yahoo.co.in>
2016-07-17 13:16:45 +0530

    setup basic structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hidden_markov',
+      version='0.1',
+      description='Implementation of Hidden markov model in discrete and continuous domains.',
+      url='http://github.com/Red-devilz/hidden_markov',
+      author='Red-devilz',
+      author_email='',
+      license='MIT',
+      packages=['hidden_markov'],
+      zip_safe=False)
